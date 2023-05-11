5e25293aeb50dfbec143b64a9a99403a47eb80c8
achyutjagini <achyut.jagini@gmail.com>
2023-05-11 14:59:44 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://mern-auth-backend-production.up.railway.app/api',
 })
 
 export const signup = payload => api.post(`/signup`, payload, {
